afa77a3b78419645c1fe140d2c192a17867af787
indramhaarta <indramhrt@gmail.com>
2023-06-01 17:18:47 +0700

    Refactor merchant handler

--- src/handler/merchantHandler/merchantHandler.ts
+++ src/handler/merchantHandler/merchantHandler.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from "express";
 import db from "../../../config/db";
+import { Location, Prisma, Seller } from "@prisma/client";
 
 export const registerMerchant = async (req: Request, res: Response) => {
   const { name, locationId, sellerId, profilePictureUrl } = req.body;
@@ -25,7 +26,7 @@ export const registerMerchant = async (req: Request, res: Response) => {
       });
     } else {
       return res.status(403).send({
-        message: `User already have merchant!}`,
+        message: `User already have merchant!`,
       });
     }
   }
@@ -55,6 +56,10 @@ export const registerMerchant = async (req: Request, res: Response) => {
 
 export const getMerchant = async (req: Request, res: Response) => {
   const { id, sellerId } = req.query;
+
+  const whereMerchant: Prisma.MerchantWhereInput = { OR: [] };
+  const whereReview: Prisma.ReviewWhereInput = {};
+
   let merchant;
 
   if (id || sellerId) {
@@ -63,7 +68,7 @@ export const getMerchant = async (req: Request, res: Response) => {
         OR: [
           { id: id as string | undefined },
           { sellerId: sellerId as string | undefined },
-        ],
+        ].filter((obj) => obj.id || obj.sellerId), // Filters out undefined values
       },
       include: { seller: true, location: true },
     });
@@ -73,13 +78,62 @@ export const getMerchant = async (req: Request, res: Response) => {
     });
   }
 
-  if (!merchant || merchant.length <= 0) {
-    return res.status(400).send({
-      message: "Merchant not found",
-    });
+  // if (id) {
+  //   whereMerchant.OR.push = [{ id: id as string }, { sellerId: id as string }];
+  //   whereReview.merchantId = id as string;
+  // }
+
+  // if (id) whereMerchant.OR.merchantId = id as string;
+  // if (id) whereMerchant.OR.sellerId = id as string;
+  if (id) whereReview.id = id as string;
+
+  // merchant = await db.merchant.findMany({
+  //   where: whereMerchant,
+  //   include: { seller: true, location: true },
+  // });
+
+  const review = await db.review.groupBy({
+    by: ["merchantId"],
+    where: whereReview,
+    _avg: { rating: true },
+    _count: { rating: true },
+  });
+
+  if (id || sellerId) {
+    if (!merchant || merchant.length <= 0) {
+      return res.status(400).send({
+        message: "Merchant not found",
+      });
+    } else {
+      const data = { ...merchant[0], rating: { ...review[0] } };
+      return res.status(200).send({ data: data });
+    }
   }
 
+  const data: {
+    rating: {};
+    id: string;
+    createdAt: Date;
+    updatedAt: Date;
+    name: string;
+    profilePictureUrl: string | null;
+    sellerId: string;
+    locationId: string;
+    seller: Seller;
+    location: Location;
+  }[] = [];
+
+  merchant.map((merchant) => {
+    const idx = review.findIndex((review) => {
+      return review.merchantId == merchant.id;
+    });
+    let rating = {};
+    if (idx >= 0) rating = { ...review[idx] };
+
+    data.push({ ...merchant, rating: rating });
+  });
+
   return res.status(200).send({
-    data: merchant,
+    data: data,
   });
 };
